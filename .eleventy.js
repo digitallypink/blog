c015151a1ffbd2aa1cba50d7aeac3186accbbe7c
UjuE <UjuE@users.noreply.github.com>
2020-04-17 08:56:22 +0100

    The experiment is coming along fine.

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,29 @@
 module.exports = (function(eleventyConfig) {
+    const flatten = arrayOfArrays => [].concat.apply([], arrayOfArrays)
+        .filter(item => item && !!item.trim());
+    const allTags = collection => flatten(collection.map(item => item.data.tags))
+
     eleventyConfig.addFilter("uniqueTags", function(collection) {
-        let arrayOfTags = collection.map(item => item.data.tags);
-        let flattenedArray = [].concat.apply([], arrayOfTags);
-        //Removing all blank tags.
-        let uniqueSetOfTags = new Set(flattenedArray.filter(item => item && !!item.trim()));
+        const uniqueSetOfTags = new Set(allTags(collection));
         return Array.from(uniqueSetOfTags);
     });
 
     eleventyConfig.addFilter("exclude", function (collection, itemToExclude) {
         return collection.filter(item => item !== itemToExclude);
+    });
+
+    eleventyConfig.addFilter("section", function (collection, section) {
+        const filter = collection.filter(item => item.data.section === section);
+        console.debug("Filter for ", section, "  ", filter);
+        return filter;
     })
+
+    eleventyConfig.addCollection("sectionNames", function (collection) {
+        return collection.getAll().map(item => item.data.section);
+    });
+
+    eleventyConfig.addCollection("topics", function (collection) {
+        return allTags(collection.getAll());
+    });
 });
+
